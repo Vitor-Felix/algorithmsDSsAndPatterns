f88acd6f93aa8a4ca8d69d4873183f12c8768d1f
Vitor-Felix <vytorfelix@gmail.com>
2022-07-29 01:55:31 -0300

    wolf - first aid kit

--- algorithms/search/linearSearch/f_linearSearch.c
+++ algorithms/search/linearSearch/f_linearSearch.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+
+// Maybe one day I handle this with dynamic memory
+#define MAX_LINES_AMOUNT 5
+#define MAX_STRING_LEN 128
+
+void convertToNumbersList(char stringList[MAX_LINES_AMOUNT][MAX_STRING_LEN]);
+void readFile(void);
 
 int main(void)
 {
-  printf("I'm ok\n");
+  readFile();
+
+  return 0;
+}
+
+void convertToNumbersList(char stringList[MAX_LINES_AMOUNT][MAX_STRING_LEN])
+{
+
+  for (int j = 0; j < MAX_LINES_AMOUNT; j++)
+  {
+    printf("%s\n", stringList[j]);
+  }
+}
+
+void readFile(void)
+{
+  FILE *pFile;
+  pFile = fopen("./../../../mocks/listOfNumbers.txt", "r");
+
+  if (pFile == NULL)
+  {
+    perror("Error opening file");
+  }
+
+  char buffer[MAX_STRING_LEN];
+  char lines[MAX_LINES_AMOUNT][MAX_STRING_LEN];
+
+  for (int i = 0; fgets(buffer, MAX_STRING_LEN, pFile) != NULL; i++)
+  {
+    strcpy(lines[i], buffer);
+  }
+  fclose(pFile);
+
+  convertToNumbersList(lines);
 }
