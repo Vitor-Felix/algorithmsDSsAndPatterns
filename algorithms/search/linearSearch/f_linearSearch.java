22f1ef7c3cc859863e7209402ba5e182ddd2ba37
Vitor-Felix <vytorfelix@gmail.com>
2022-08-02 23:07:53 -0300

    狂感できない - 88Kasyo Junrei

--- algorithms/search/linearSearch/f_linearSearch.java
+++ algorithms/search/linearSearch/f_linearSearch.java
@@ -1,5 +1,14 @@
 public class f_linearSearch {
   public static void main(String[] args) {
-    System.out.println("I'm ok, I'm ok");
+    int[] unsortedNumbers = { 2, 3, 1, 10, 7, 8 };
+    int target = 100;
+    boolean found = false;
+
+    for (int i = 0; i < unsortedNumbers.length; i++) {
+      if (unsortedNumbers[i] == target) {
+        found = true;
+      }
+    }
+    System.out.println(found);
   }
 }
