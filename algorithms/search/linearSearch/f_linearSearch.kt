d0d98956decc2dc54c8e0af6ab67432871f251c7
Vitor Felix <vytorfelix@gmail.com>
2022-10-08 02:47:05 -0300

    Free Bird - Lynyrd Skynyrd

--- algorithms/search/linearSearch/f_linearSearch.kt
+++ algorithms/search/linearSearch/f_linearSearch.kt
@@ -1,5 +1,17 @@
-class HelloWorldKt {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+import java.time.Duration
+import java.time.Instant
+
+fun main() {
+    val unsortedNumbers = intArrayOf(2, 3, 1, 10, 7, 8)
+    val target = 2
+    var found = false
+
+    val clockStart = Instant.now();
+    for (number in unsortedNumbers) {
+        if (number == target) { found = true }
     }
+    val clockEnd = Instant.now()
+
+    println(found)
+    println("Elapsed time: " + Duration.between(clockEnd, clockStart).toString())
 }
